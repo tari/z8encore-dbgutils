9d0def89125048b7117c544e6e1b706143b34945
jnekl <jnekl>
2005-10-20 18:40:40 +0000

    Update for new devices - Motor control and XP.

--- ez8dbg.h
+++ ez8dbg.h
@@ -1,6 +1,6 @@
 /* Copyright (C) 2002, 2003, 2004 Zilog, Inc.
  *
- * $Id: ez8dbg.h,v 1.3 2004/12/01 22:42:33 jnekl Exp $
+ * $Id: ez8dbg.h,v 1.4 2005/10/20 18:40:40 jnekl Exp $
  *
  * This implements a debugger api for the ez8 on-chip debugger.
  */
@@ -16,17 +16,19 @@
 /**************************************************************/
 
 /* cache status */
-#define	DBGREV_CACHED	0x01
-#define	DBGCTL_CACHED	0x02
-#define	DBGSTAT_CACHED	0x04
-#define	PC_CACHED	0x08
-#define	CRC_CACHED	0x10
-#define	MEMCRC_CACHED	0x20
-#define	MEMSIZE_CACHED	0x40
-#define	FCTL_CACHED	0x80
-#define	RELOAD_CACHED	0x0100
-#define	SYSCLK_CACHED	0x0200
-#define	FREQ_CACHED	0x0400
+#define	REVID_CACHED	0x0001
+#define	DBGCTL_CACHED	0x0002
+#define	DBGSTAT_CACHED	0x0004
+#define	PC_CACHED	0x0008
+#define	CRC_CACHED	0x0010
+#define	MEMCRC_CACHED	0x0020
+#define	MEMSIZE_CACHED	0x0040
+#define	FCTL_CACHED	0x0080
+#define	BAUDRATE_CACHED	0x0100
+#define	RELOAD_CACHED	0x0200
+#define	SYSCLK_CACHED	0x0400
+#define	FREQ_CACHED	0x0800
+#define	TIMEOUT_CACHED	0x1000
 
 /* 5 second reset timeout (typical reset is 10ms) */
 #define	RESET_TIMEOUT	5
@@ -43,7 +45,7 @@ private:
 	ez8dbg(ez8dbg &);	
 
 	/* cached data */
-	uint16_t dbgrev;
+	uint16_t revid;
 	uint8_t dbgctl;
 	uint8_t dbgstat;
 	uint16_t pc;
@@ -52,6 +54,8 @@ private:
 	uint8_t memsize;
 	uint16_t reload;
 	uint16_t freq;
+	int baudrate;
+	int timeout;
 
 	/* buffers */
 	uint8_t *main_mem;
@@ -70,16 +74,12 @@ private:
 	void delete_breakpoint(int);
 
 	/* internal functions */
-	void cache_dbgrev(void);
-	void cache_dbgctl(void);
-	void cache_dbgstat(void);
-	void cache_pc(void);
-	void cache_crc(void);
-	void cache_memcrc(void);
-	void cache_memsize(void);
-	void cache_reload(void);
-	void cache_sysclk(void);
+	uint8_t  cached_dbgctl(void);
+	uint8_t  cached_dbgstat(void);
+	uint16_t cached_memcrc(void);
+	uint8_t  cached_memsize(void);
 	void cache_freq(void);
+	void set_timeout(void);
 
 public:
 	int sysclk;
@@ -117,9 +117,11 @@ public:
 	void step(void);
 	void next(void);
 
-	uint16_t rd_cntr(void);
+	uint16_t rd_revid(void);
 	uint16_t rd_crc(void);
 	uint16_t rd_pc(void);
+	uint16_t rd_cntr(void);
+	uint16_t rd_reload(void);
 	void wr_pc(uint16_t);
 	void rd_regs(uint16_t, uint8_t *, size_t);
 	void wr_regs(uint16_t, const uint8_t *, size_t);
@@ -144,7 +146,12 @@ public:
 	void read_mem(uint16_t, uint8_t *, size_t);
 
 	int memory_size(void);
-	int get_sysclk(void);
+	int cached_baudrate(void);
+	int cached_sysclk(void);
+	uint16_t cached_revid(void);
+	uint16_t cached_crc(void);
+	uint16_t cached_reload(void);
+	uint16_t cached_pc(void);
 
 	uint8_t rd_trce_status(void);
 	void wr_trce_ctl(uint8_t);	
